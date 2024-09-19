e7a1503d7ccc350b8033f00cfcd3d11e072bdc09
aliasr2001 <asr.studio2001@gmail.com>
2024-09-19 16:45:53 -0600

    created the login page and coded the Page Structure

---
+++ src/app/login/page.tsx
@@ -0,0 +1,21 @@
+export default function LoginPage () {
+    return(
+        <main>
+        <header>
+            <nav className="navbar">
+                <div className="logoCont"></div>
+                <div className="links"></div>
+            </nav>
+        </header>
+        <main>
+            <div className="main-content">
+                <div className="loginBtn"></div>
+                <div className="arrowSvg"></div>
+            </div>
+        </main>
+        <footer>
+            <div className="footer-content"></div>
+        </footer>
+        </main>
+    );
+}
