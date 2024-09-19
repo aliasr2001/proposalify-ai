9222bf091acbc53328ad5afa7198fa92cf441f05
SHzTripleFouR <mrshztriplefour@gmail.com>
2024-09-20 04:45:45 +0500

    Created a template

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+export default function loginPage() {
+    return(
+       <main>
+        <header>
+            <nav className="">
+                
+            </nav>
+        </header>
+        <main></main>
+        <footer></footer>
+       </main>
+    );
+}
